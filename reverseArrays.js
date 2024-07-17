161cb9b38722a81be50d2f946f184fb8ff192f52
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-07-16 20:07:55 -0700

    worked on reversing arrays in place

--- reverseArrays.js
+++ reverseArrays.js
@@ -1,8 +1,33 @@
 
 // reversing by creating new array
 // relatively easy
+let newArr = [];
+function myReverse(arr){
+    for(let i = arr.length - 1; i >= 0; i--){
+        newArr.push(arr[i])
+    }
+    return newArr;
+}
 
-let myArr = [1,2,3,4,5];
+
+
+poop = [1,2,3,4,5];
+
+
+function reverseInPlace(arr){
+    let leftIndex = 0;
+    let rightIndex = arr.length -1;
+
+    while(leftIndex < rightIndex){
+        let temp;
+        temp = arr[rightIndex];
+        arr[rightIndex] = arr[leftIndex];
+        arr[leftIndex] = temp;
+        leftIndex += 1;
+        rightIndex -= 1
+    }
+    return arr;
+}
 
 
 
